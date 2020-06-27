724424fe0180433d9285e8b3f41d0ebfdd7e8f96
Adriano Xavier <px.adriano@gmail.com>
2020-06-27 17:08:46 -0300

    [MOD]: logo

--- lib/brcobranca/boleto/base.rb
+++ lib/brcobranca/boleto/base.rb
@@ -88,6 +88,8 @@ module Brcobranca
       attr_accessor :avalista_documento
       # <b>OPCIONAL</b>: Endereço do beneficiário
       attr_accessor :cedente_endereco
+      # <b>OPCIONAL</b>: Logo Empresa
+      attr_accessor :logo_empresa
 
       # Validações
       validates_presence_of :agencia, :conta_corrente, :moeda, :especie_documento, :especie, :aceite, :nosso_numero, :sacado, :sacado_documento, message: 'não pode estar em branco.'
